4199be6824564fa7600c0cafde5a4cee2401a9fb
kewerson.hugo <kewerson.hugo@gmail.com>
2017-10-28 07:19:38 -0200

    Criado README e modificado outros arquivos

---
+++ README.md
@@ -0,0 +1,21 @@
+# ALAE - AccessibilityLessAsError
+
+Resumo
+
+## Instalação
+
+Clone o repositório:
+```
+git clone https://github.com/alanepontes/alae-app.git
+cd alae-app
+```
+
+Instale as dependencias:
+```
+npm install
+```
+
+Adicione o report gerado pelo pa11y e rode o seguinte comando:
+```
+npm run build
+```
--- mapper.js
+++ mapper.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 const obj = JSON.parse(fs.readFileSync('report.json', 'utf8'));
 const mappedObj = obj.map(item => {
     const code = item.code.split('.');
-    const standardName = item.code[0];
-    const principle = item.code[1];
-    const guideline = item.code[2];
-    const parsedCriteria = code[3].replace(/_/g,'.');
-    const techniques = code[4].split(',');
+    item.standardName = item.code[0];
+    item.principle = item.code[1];
+    item.guideline = item.code[2];
+    item.parsedCriteria = code[3].replace(/_/g,'.');
+    item.techniques = code[4].split(',');
     item.affecteds = ['visao','audicao'];
-    item.techniques = techniques;
     return item;
 });
 
