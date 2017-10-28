a4844f6fc98f83da88d4beb6083c02662d631d59
kewerson.hugo <kewerson.hugo@gmail.com>
2017-10-28 07:58:45 -0200

    Atualizado estrutura de pastas e gitignore

--- config.js
+++
@@ -1,4 +0,0 @@
-pa11y({
-    allowedStandards: ['WCAG2A','WCAG2AA', 'WCAG2AAA'],
-    standard: 'Section508'
-});
--- src/mapper.js
+++ src/mapper.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const fa11yReport = JSON.parse(fs.readFileSync('report.json', 'utf8'));
-const deficienciesInCriterias = JSON.parse(fs.readFileSync('./mappers/guidelines.json', 'utf8'));
+const fa11yReport = JSON.parse(fs.readFileSync('./src/report/report.json', 'utf8'));
+const deficienciesInCriterias = JSON.parse(fs.readFileSync('./src/mappers/guidelines.json', 'utf8'));
 const mappedCriteriaReport = fa11yReport.map(item => {
     const code = item.code.split('.');
     item.standardName = code[0];
@@ -12,4 +12,4 @@ const mappedCriteriaReport = fa11yReport.map(item => {
     return item;
 });
 
-fs.writeFileSync('result.json', JSON.stringify(mappedCriteriaReport));
+fs.writeFileSync('./dist/result.json', JSON.stringify(mappedCriteriaReport));
