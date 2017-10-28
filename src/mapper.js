0982ace55cfb6f49fe2e6c0d505dc423a0d5ca8d
alane <alanepontes@gmail.com>
2017-10-28 08:32:19 -0200

    created reports datas

--- src/mapper.js
+++ src/mapper.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
-const fa11yReport = JSON.parse(fs.readFileSync('./src/report/report.json', 'utf8'));
-const deficienciesInCriterias = JSON.parse(fs.readFileSync('./src/mappers/guidelines.json', 'utf8'));
-const mappedCriteriaReport = fa11yReport.map(item => {
+const report = JSON.parse(fs.readFileSync('./src/report/report.json', 'utf8'));
+
+const deficienciesPercentPopulation = JSON.parse(fs.readFileSync('./src/mappers/deficienciesPercentPopulation.json', 'utf8'));
+
+const deficienciesByGuideline = JSON.parse(fs.readFileSync('./src/mappers/deficienciesByGuideline.json', 'utf8'));
+const deficienciesTotalByGuideline = JSON.parse(fs.readFileSync('./src/mappers/deficienciesTotalByGuideline.json', 'utf8'));
+
+const totalUsersInApplication = JSON.parse(fs.readFileSync('./src/mappers/totalUsersInApplication.json', 'utf8'));
+
+
+
+const mappedCriteriaReport = report.map(item => {
     const code = item.code.split('.');
     item.standardName = code[0];
     item.principle = code[1];
     item.guideline = code[2];
     item.parsedCriteria = code[3].replace(/_/g,'.');
     item.techniques = code[4].split(',');
-    item.affecteds = deficienciesInCriterias[item.parsedCriteria.toString()];
+    item.affecteds = deficienciesByGuideline[item.parsedCriteria.toString()];
     return item;
 });
 
