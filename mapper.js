239acc0954c53127092a056f28558ccb9d515779
kewerson.hugo <kewerson.hugo@gmail.com>
2017-10-28 07:33:07 -0200

    Alterado o mapper.js e mapper.json

--- mapper.js
+++ mapper.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
-const obj = JSON.parse(fs.readFileSync('report.json', 'utf8'));
-const mappedObj = obj.map(item => {
+const fa11yReport = JSON.parse(fs.readFileSync('report.json', 'utf8'));
+const deficiesInCriterias = JSON.parse(fs.readFileSync('mapper.json', 'utf8'));
+const mappedCriteriaReport = fa11yReport.map(item => {
     const code = item.code.split('.');
-    item.standardName = item.code[0];
-    item.principle = item.code[1];
-    item.guideline = item.code[2];
+    item.standardName = code[0];
+    item.principle = code[1];
+    item.guideline = code[2];
     item.parsedCriteria = code[3].replace(/_/g,'.');
     item.techniques = code[4].split(',');
-    item.affecteds = ['visao','audicao'];
+    item.affecteds = deficiesInCriterias[item.parsedCriteria.toString()];
     return item;
 });
 
-fs.writeFileSync('result.json', JSON.stringify(mappedObj));
+fs.writeFileSync('result.json', JSON.stringify(mappedCriteriaReport));
